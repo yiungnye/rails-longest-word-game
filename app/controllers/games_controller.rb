ea99f487fdfdf9339cc3411b864d461431a39017
Jeremy Chan Yue Nang <yiungnye@gmail.com>
2023-02-20 17:57:36 +0400

    third exercise done

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,35 @@
+require 'open-uri'
+require 'nokogiri'
+
+class GamesController < ApplicationController
+  def new
+    @letters = [*('A'..'Z')].sample(10)
+  end
+
+  def score
+    # raise
+    @word = params[:word]
+    @words = @word.upcase.split("")
+    @letters = params[:selected_letters]
+    @letterss = @letters.split
+
+    if !(@letterss & @words).any? && english_word?(@word) == true
+      @result = "Congratulations! #{@word} is a valid English word"
+    elsif @word == ''
+      @result = 'Input a word to play'
+    elsif english_word?(@word) == false
+      @result = "Sorry but #{@word} does not seem to be a valid English word"
+    else
+      @result = "Sorry but TEST can\'t be built out of #{@letters.gsub(" ", ", ")}"
+    end
+  end
+
+  private
+
+  def english_word?(word)
+    url = "https://wagon-dictionary.herokuapp.com/#{word}"
+    html = URI.open(url).read
+    user = JSON.parse(html)
+    user["found"]
+  end
+end
